d4597e408524359ecc15bc821f9b12e70e8137fc
saragomec <47632330+saragomec@users.noreply.github.com>
2020-11-15 09:03:48 +0100

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,67 @@
+package es.unileon.prg1.date;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date (int day, int month, int year) throws DateException {
+		//this.month = month;
+		this.setMonth(month);
+		//this.day = day;
+		this.setDay(day);
+		//this.year = year;
+		this.setYear(year);
+	}
+	
+	public void setDay(int day) throws DateException {
+		if ( day < 1 || day > this.getDaysOfMonth() ) {
+			throw new DateException("Date error: Day " + day + " of month " + this.month + " not valid");			
+		}
+		this.day = day;
+	}
+	
+	public void setMonth (int month) throws DateException {
+		if ( month < 1 || month > 12) {
+			throw new DateException("Date error: Month " + month + " not valid");
+		}
+		this.month = month;
+	}
+	
+	public void setYear (int year) {
+		this.year = year;
+	}
+	
+	private int getDaysOfMonth() {
+		int numDays;
+		
+		numDays = 0;
+		switch (this.month) {
+		case 1: //next
+		case 3: //next
+		case 5: //next
+		case 7: //next
+		case 8: //next
+		case 10: //next
+		case 12:
+			numDays = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			numDays = 30;
+			break;
+		case 2:
+			numDays = 28;
+			break;			
+		}
+		
+		return numDays;
+	}
+	
+	public String toString() {
+		return this.day + "/" + this.month + "/" + this.year;
+	}
+
+}
